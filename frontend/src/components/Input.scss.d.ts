67d2512118c4ba1d08b520ffe47f794f73886b65
dominikzeglen <flesz3@o2.pl>
2022-11-23 18:54:19 +0100

    Fix input styles

--- frontend/src/components/Input.scss.d.ts
+++ frontend/src/components/Input.scss.d.ts
@@ -3,3 +3,4 @@
 /* eslint-disable */
 export const root: string;
 export const label: string;
+export const fullWidth: string;
--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -2,28 +2,30 @@ import clsx from "clsx";
 import React from "react";
 import styles from "./Input.scss";
 
-export const Input = React.forwardRef<
-  HTMLInputElement,
-  React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >
->((props, ref) => (
-  <input
-    {...props}
-    ref={ref}
-    // eslint-disable-next-line react/destructuring-assignment
-    className={clsx(styles.root, props?.className)}
-  />
-));
+export type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  fullWidth?: boolean;
+};
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => (
+    <input
+      {...props}
+      ref={ref}
+      // eslint-disable-next-line react/destructuring-assignment
+      className={clsx(styles.root, props?.className, {
+        [styles.fullWidth]: props.fullWidth,
+      })}
+    />
+  )
+);
 Input.displayName = "Input";
 
 export const LabeledInput = React.forwardRef<
   HTMLInputElement,
-  React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > & { label: string }
+  InputProps & { label: string }
 >(({ id, label, ...props }, ref) => (
   <div>
     <label className={styles.label} htmlFor={id}>
