778a2408b567a1edf7fee57de25fac7b585eaf25
dominikzeglen <flesz3@o2.pl>
2022-12-08 16:47:33 +0200

    Change build directory

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -62,7 +62,7 @@ const config = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js",
   },
