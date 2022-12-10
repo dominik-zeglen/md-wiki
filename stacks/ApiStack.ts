1fb2b661189a04381203ba1cfcaca4fe38f5add6
dominikzeglen <flesz3@o2.pl>
2022-12-10 01:58:30 +0100

    Use lambda constructor

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,4 +1,4 @@
-import { Api, StackContext, use } from "@serverless-stack/resources";
+import { Api, StackContext, use, Function } from "@serverless-stack/resources";
 import { AuthStack } from "./AuthStack";
 import { StorageStack } from "./StorageStack";
 
@@ -6,23 +6,24 @@ export function ApiStack({ stack, app }: StackContext) {
   const { auth } = use(AuthStack);
   const { cluster } = use(StorageStack);
 
+  const trpc = new Function(stack, "trpcHandler", {
+    permissions: [cluster, "cognito-idp:*"],
+    handler: "api/index.main",
+    environment: {
+      REGION: app.region,
+      DATABASE_NAME: cluster.defaultDatabaseName,
+      SECRET_ARN: cluster.secretArn,
+      CLUSTER_ARN: cluster.clusterArn,
+      COGNITO_POOL_ID: auth.userPoolId,
+    },
+  });
+
   const api = new Api(stack, "Api", {
     cors: true,
-    defaults: {
-      function: {
-        permissions: [cluster],
-        environment: {
-          REGION: app.region,
-          DATABASE_NAME: cluster.defaultDatabaseName,
-          SECRET_ARN: cluster.secretArn,
-          CLUSTER_ARN: cluster.clusterArn,
-          COGNITO_POOL_ID: auth.userPoolId,
-        },
-      },
-    },
+    defaults: {},
     routes: {
-      "GET /trpc/{proxy+}": "api/index.main",
-      "POST /trpc/{proxy+}": "api/index.main",
+      "GET /trpc/{proxy+}": trpc,
+      "POST /trpc/{proxy+}": trpc,
     },
   });
 
