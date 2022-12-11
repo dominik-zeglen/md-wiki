30a6f19ceb5e940517728f1d4a19175c81dfa4f2
dominikzeglen <flesz3@o2.pl>
2022-12-11 23:34:41 +0100

    wip

--- services/api/pages.ts
+++ services/api/pages.ts
@@ -9,6 +9,31 @@ import {
   updatePage,
 } from "../repository/page";
 import { jwtMiddleware } from "./middlewares/jwt";
+import { remark } from "remark";
+import directivePlugin from "remark-directive";
+import { visit } from "unist-util-visit";
+import { pipe, toArray, uniq } from "@fxts/core";
+
+function pageReferencePlugin(cb: (slug: string) => void) {
+  return () => (tree) => {
+    visit(tree, (node) => {
+      if (
+        node.type === "textDirective" ||
+        node.type === "leafDirective" ||
+        node.type === "containerDirective"
+      ) {
+        if (node.name !== "page") return;
+
+        const attributes = node.attributes || {};
+        const slug = attributes.id;
+
+        if (!slug) return;
+
+        cb(slug);
+      }
+    });
+  };
+}
 
 export const pageRouter = router({
   get: procedure
@@ -51,6 +76,12 @@ export const pageRouter = router({
     .use(jwtMiddleware)
     .mutation((req) => {
       const user = req.ctx.user!.userName;
+      let slugs: string[] = [];
+
+      remark()
+        .use([directivePlugin, pageReferencePlugin((slug) => slugs.push(slug))])
+        .processSync(req.input.input.content);
+      const uniqueSlugs = pipe(slugs, uniq, toArray);
 
       return updatePage({ user, slug: req.input.slug, data: req.input.input });
     }),
