6f2ec7d5893db21a563fd9f1f80fbac3ca4f77da
dominikzeglen <flesz3@o2.pl>
2022-12-27 00:12:47 +0100

    Add timeline component

--- frontend/src/components/PagePreview.scss.d.ts
+++ frontend/src/components/PagePreview.scss.d.ts
@@ -4,4 +4,5 @@
 export const root: string;
 export const image: string;
 export const imageRight: string;
+export const timeline: string;
 export const clearFix: string;
--- frontend/src/components/PagePreview.tsx
+++ frontend/src/components/PagePreview.tsx
@@ -12,70 +12,108 @@ export interface PagePreviewProps {
   page: AppRouterOutputs["pages"]["get"] | undefined;
 }
 
-function embedPagePlugin() {
-  return (tree) => {
-    visit(tree, (node) => {
-      if (
-        node.type === "textDirective" ||
-        node.type === "leafDirective" ||
-        node.type === "containerDirective"
-      ) {
-        if (node.name !== "page") return;
+type Plugin = typeof directivePlugin;
 
-        const data = node.data || (node.data = {});
-        const attributes = node.attributes || {};
-        const slug = attributes.id;
+const embedPagePlugin: Plugin = () => (tree) => {
+  visit(tree, (node) => {
+    if (node.type === "textDirective") {
+      if (node.name !== "page") return;
 
-        if (!slug) return;
+      const data = node.data || (node.data = {});
+      const attributes = node.attributes || {};
+      const slug = attributes.id;
 
-        data.hName = "a";
-        data.hProperties = {
-          href: siteRoutes.page.to({ slug }),
-        };
-      }
-    });
-  };
-}
+      if (!slug) return;
 
-function embedImagePlugin() {
-  return (tree) => {
-    visit(tree, (node) => {
-      if (node.type === "textDirective") {
-        if (node.name !== "img") return;
+      data.hName = "a";
+      data.hProperties = {
+        href: siteRoutes.page.to({ slug }),
+      };
+    }
+  });
+};
 
-        const data = node.data || (node.data = {});
-        const attributes = node.attributes || {};
-        const alt = attributes.alt;
-        const src = attributes.src;
-        const side = attributes.side;
-        const className = clsx(styles.image, {
-          [styles.imageRight]: side === "right",
-        });
+const embedImagePlugin: Plugin = () => (tree) => {
+  visit(tree, (node) => {
+    if (node.type === "textDirective") {
+      if (node.name !== "img") return;
 
-        const inlineStyles = [
-          attributes.width ? `width: ${attributes.width}px;` : null,
-          attributes.height ? `height: ${attributes.height}px;` : null,
-        ]
-          .filter(Boolean)
-          .join();
+      const data = node.data || (node.data = {});
+      const attributes = node.attributes || {};
+      const alt = attributes.alt;
+      const src = attributes.src;
+      const side = attributes.side;
+      const className = clsx(styles.image, {
+        [styles.imageRight]: side === "right",
+      });
 
-        if (!src) return;
+      const inlineStyles = [
+        attributes.width ? `width: ${attributes.width}px;` : null,
+        attributes.height ? `height: ${attributes.height}px;` : null,
+      ]
+        .filter(Boolean)
+        .join();
 
-        node.children = [];
-        data.innerHTML = "";
-        data.hName = "img";
-        data.hProperties = {
-          alt,
-          src,
-          className,
-          style: inlineStyles,
-        };
-      }
-    });
-  };
-}
+      if (!src) return;
+
+      node.children = [];
+      data.innerHTML = "";
+      data.hName = "img";
+      data.hProperties = {
+        alt,
+        src,
+        className,
+        style: inlineStyles,
+      };
+    }
+  });
+};
+
+const timelinePlugin: Plugin = () => (tree) => {
+  visit(tree, (node) => {
+    if (node.type === "containerDirective") {
+      if (node.name !== "timeline") return;
+
+      const data = node.data || (node.data = {});
+
+      data.hName = "dl";
+      data.hProperties = {
+        className: styles.timeline,
+      };
+    }
+
+    if (node.type === "leafDirective") {
+      if (node.name !== "tdate") return;
+
+      const data = node.data || (node.data = {});
+      const attributes = node.attributes || {};
+      const { date } = attributes;
+
+      if (!date) return;
+      data.hName = "tdate";
+      data.hProperties = {
+        date,
+      };
+    }
+  });
+};
 
 export const PagePreview: React.FC<PagePreviewProps> = ({ page }) => {
+  const components = React.useMemo<Record<string, any>>(
+    () => ({
+      /* eslint-disable react/no-unstable-nested-components */
+      a: ({ href, children }) => <Link to={href!}>{children}</Link>,
+      tdate: ({ children, node }) => (
+        <>
+          <dt>{node.properties.date}</dt>
+          <dd>{children}</dd>
+        </>
+      ),
+      /* eslint-enable react/no-unstable-nested-components */
+    }),
+    []
+  );
+
   if (!page) {
     return null;
   }
@@ -84,11 +122,13 @@ export const PagePreview: React.FC<PagePreviewProps> = ({ page }) => {
     <>
       <div className={styles.root}>
         <ReactMarkdown
-          remarkPlugins={[directivePlugin, embedPagePlugin, embedImagePlugin]}
-          components={{
-            // eslint-disable-next-line react/no-unstable-nested-components
-            a: ({ href, children }) => <Link to={href!}>{children}</Link>,
-          }}
+          remarkPlugins={[
+            directivePlugin,
+            embedPagePlugin,
+            embedImagePlugin,
+            timelinePlugin,
+          ]}
+          components={components}
         >
           {page?.content!}
         </ReactMarkdown>
