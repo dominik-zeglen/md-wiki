abe8342639efedd7d2b5c7e921ebce160b01f0cf
dominikzeglen <flesz3@o2.pl>
2023-01-09 23:31:16 +0100

    Add search box to pages

--- frontend/src/pages/panel/PageList.tsx
+++ frontend/src/pages/panel/PageList.tsx
@@ -11,8 +11,11 @@ import StarIcon from "@assets/star.svg";
 import StarOutlinedIcon from "@assets/starOutlined.svg";
 import { IconButton } from "src/components/IconButton";
 import { PanelHeader } from "src/components/PanelHeader";
-import styles from "./PageList.scss";
+import { Input } from "src/components/Input";
+import { useQs } from "src/hooks/useQs";
+import debounce from "lodash/debounce";
 import { PageLoading } from "../common/PageLoading";
+import styles from "./PageList.scss";
 
 export interface PageProps {
   pages: AppRouterOutputs["pages"]["list"] | undefined;
@@ -20,58 +23,72 @@ export interface PageProps {
   onHighlight: (input: AppRouterInputs["pages"]["highlight"]) => void;
 }
 
-export const PageList: React.FC<PageProps> = ({ pages, onHighlight }) => (
-  <div>
-    <PanelHeader title="Pages">
-      <Link to={panelRoutes.pageCreate.to()}>
-        <Button>New page</Button>
-      </Link>
-    </PanelHeader>
+export const PageList: React.FC<PageProps> = ({ pages, onHighlight }) => {
+  const [{ title }, setParams] = useQs();
 
-    {pages === undefined ? (
-      <PageLoading />
-    ) : (
-      <div className={styles.grid}>
-        <div>
-          <div className={styles.item}>
-            <span>Page name</span>
-            <span>Last edited</span>
-          </div>
-          {pages.results.map((page) => (
-            <Card
-              className={clsx(styles.item, {
-                [styles.itemHighlighted]: page.highlighted,
-              })}
-              key={page.slug}
-            >
-              <Link to={panelRoutes.page.to({ slug: page.slug })}>
-                {page.title}
-              </Link>
-              {Intl.DateTimeFormat(undefined, {
-                dateStyle: "medium",
-                timeStyle: "short",
-              }).format(dbDateToDateObject(page.updatedAt as string))}
-              <IconButton
-                onClick={() =>
-                  onHighlight({
-                    slug: page.slug,
-                    highlighted: !page.highlighted,
-                  })
-                }
-                variant="flat"
+  return (
+    <div>
+      <PanelHeader title="Pages">
+        <Link to={panelRoutes.pageCreate.to()}>
+          <Button>New page</Button>
+        </Link>
+      </PanelHeader>
+
+      {pages === undefined ? (
+        <PageLoading />
+      ) : (
+        <div className={styles.grid}>
+          <div>
+            <Input
+              autoFocus
+              fullWidth
+              placeholder="Search pages..."
+              defaultValue={title}
+              onChange={debounce(
+                (event) => setParams({ title: event.target.value, page: "1" }),
+                300
+              )}
+            />
+            <div className={styles.item}>
+              <span>Page name</span>
+              <span>Last edited</span>
+            </div>
+            {pages.results.map((page) => (
+              <Card
+                className={clsx(styles.item, {
+                  [styles.itemHighlighted]: page.highlighted,
+                })}
+                key={page.slug}
               >
-                {page.highlighted ? (
-                  <StarIcon className={styles.itemHighlightedIcon} />
-                ) : (
-                  <StarOutlinedIcon className={styles.itemHighlightedIcon} />
-                )}
-              </IconButton>
-            </Card>
-          ))}
-          <Pagination />
+                <Link to={panelRoutes.page.to({ slug: page.slug })}>
+                  {page.title}
+                </Link>
+                {Intl.DateTimeFormat(undefined, {
+                  dateStyle: "medium",
+                  timeStyle: "short",
+                }).format(dbDateToDateObject(page.updatedAt as string))}
+                <IconButton
+                  onClick={() =>
+                    onHighlight({
+                      slug: page.slug,
+                      highlighted: !page.highlighted,
+                    })
+                  }
+                  variant="flat"
+                >
+                  {page.highlighted ? (
+                    <StarIcon className={styles.itemHighlightedIcon} />
+                  ) : (
+                    <StarOutlinedIcon className={styles.itemHighlightedIcon} />
+                  )}
+                </IconButton>
+              </Card>
+            ))}
+            <Pagination />
+          </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+};
 PageList.displayName = "PageList";
